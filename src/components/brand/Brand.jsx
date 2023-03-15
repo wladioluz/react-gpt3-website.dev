e499815f5f7a9420c98c3c32cbba679d5d00edd0
Wlad <w.pylypczuk@gmail.com>
2023-03-15 15:30:39 +0100

    features and possibilities sections

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './brand.css';
+import { google, slack, dropbox, shopify, atlassian } from './imports';
 
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google logo" /> 
+      </div>
+      <div>
+        <img src={slack} alt="slack logo" /> 
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox logo" /> 
+      </div>
+      <div>
+        <img src={shopify} alt="shopify logo" /> 
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian logo" /> 
+      </div>
     </div>
   )
 }
