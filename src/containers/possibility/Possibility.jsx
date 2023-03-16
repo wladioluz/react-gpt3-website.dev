62044c498443e2b982595795ac78ed2087023ad8
Wlad <w.pylypczuk@gmail.com>
2023-03-16 17:22:59 +0100

    RWD main image

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>
-        Possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1 className='gradient__text'>The possibilites are beyond your imagination</h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus.</p>
+        <h4>Request early access to get started</h4>
+      </div>
     </div>
   )
 }
 
-export default Possibility
+export default Possibility;
