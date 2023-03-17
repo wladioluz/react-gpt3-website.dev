509cef51c245bd9f29d82ca12e1e183ecca73265
Wlad <w.pylypczuk@gmail.com>
2023-03-17 12:23:35 +0100

    footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import gpt3logo from '../../assets/logo.svg'
 import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links__logo'>
+          <img src={gpt3logo} alt="logo" />
+          <p>© 2023 GPT-3. All rights reserved.</p>
+        </div>
+        <div className='gpt3__footer-links__div'>
+          <h4>Links</h4>
+          <p>Contact</p>
+          <p>Contact</p>
+          <p>Contact</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links__div'>
+          <h4>Company</h4>
+          <p>Contact</p>
+          <p>Contact</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links__div'>
+          <h4>Get in touch</h4>
+          <p>Contact</p>
+          <p>Contact</p>
+          <p>Contact</p>
+          <p>Contact</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2023 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
